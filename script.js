69c1b857c134a8accba88e74a144ec150d01d63d
kaushal1999 <kaushalwaghela1999@gmail.com>
2023-04-13 21:07:05 +0530

    completed api part

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
-const options = {
-    method: "GET",
-    
-//   url: "https://andruxnet-random-famous-quotes.p.rapidapi.com/",
-  headers: {
-    "X-RapidAPI-Key": "6bf3cfed4amshafb31367b1ad446p100b97jsn780e248656cc",
-    "X-RapidAPI-Host": "andruxnet-random-famous-quotes.p.rapidapi.com",
-  },
+
+
+let joke;
+let texts = document.querySelectorAll(".text");
+let front = 1;
+
+let newQuote = () => {
+    let fetchRes = fetch(
+      "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single"
+    );
+
+    fetchRes
+      .then((resp) => {
+        return resp.json();
+      })
+      .then((val) => {
+        joke = val["joke"]+" ";
+        texts[front].innerHTML = joke;
+        front = 1 - front;
+      });
+  
+  let box1 = document.querySelector(".block__front");
+  let box2 = document.querySelector(".block__back");
+  box1.classList.remove("block__front");
+  box2.classList.remove("block__back");
+  box1.classList.add("block__back");
+  box2.classList.add("block__front");
 };
 
-let fetchRes = fetch("https://rapidapi.com", options);
-
-fetchRes.then((response) => {
-    console.log(response.status);
-})
+newQuote();
